9c1da0bba6ae35cf10e84890cdda473ff4f97cbb
unknown <akpos97@ymail.com>
2026-05-08 22:54:17 +0100

    Fix: Use correct (custom) URL for project repository deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://merve322.github.io/merveillenaomi',
+  site: 'https://merveillenaomi.com',
 });
